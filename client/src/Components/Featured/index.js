52a02509d106eb9bb60dded4c143285c173310e4
dchicchon <danielchicchon@gmail.com>
2020-02-08 11:54:19 -0800

    removed fixed height for featured component

--- client/src/Components/Featured/index.js
+++ client/src/Components/Featured/index.js
@@ -23,51 +23,95 @@ class Featured extends React.Component {
   };
 
   componentDidMount() {
-    // const screenWidth = window.screen.width;
+    const screenWidth = window.screen.width;
     // Change photo used depending on the screen size
-    // if (screenWidth > 400) {
-    this.setState({
-      projectList: [
-        {
-          image: gittrack,
-          name: "GitTrack",
-          link: "http://www.gittrack.ml/",
-          authors: "dchicchon",
-          repo: "https://github.com/dchicchon/GitTrack",
-          description:
-            " A commit tracking site for instructors to keep track of student progress.",
-          caption: "React.js, MySQL, Victory.js, Passport.js"
-        },
-        {
-          image: plannit,
-          name: "PlanIt",
-          link: "http://planit.ml/",
-          authors: "esarnb, minalk24, matkuh, dchicchon",
-          repo: "https://github.com/dchicchon/Plan-It",
-          description: "A planner app that helps keep track of your day",
-          caption: "Firebase, APIs, JQuery, HTML5, CSS"
-        },
-        {
-          image: spaceTrivia,
-          name: "Space Trivia",
-          link: "https://spacetrivia.herokuapp.com/",
-          authors: "dchicchon",
-          repo: "",
-          description: "Test your knowledge of the Solar System!",
-          caption: "JQuery, HMTL5, CSS, MongoDB"
-        },
-        {
-          image: "",
-          name: "Planner",
-          link:
-            "https://chrome.google.com/webstore/detail/planner/meajimhgfmioppbkoppphhkbcmapfngh",
-          authors: "dchicchon",
-          repo: "https://github.com/dchicchon/planner_extension",
-          description: "A planner to help plan your future!",
-          caption: "HTML5, CSS, Chrome Dev Tools"
-        }
-      ]
-    });
+    if (screenWidth > 400) {
+      this.setState({
+        projectList: [
+          {
+            image: gittrack,
+            name: "GitTrack",
+            link: "http://www.gittrack.ml/",
+            authors: "dchicchon",
+            repo: "https://github.com/dchicchon/GitTrack",
+            description:
+              " A commit tracking site for instructors to keep track of student progress.",
+            caption: "React.js, MySQL, Victory.js, Passport.js"
+          },
+          {
+            image: plannit,
+            name: "PlanIt",
+            link: "http://planit.ml/",
+            authors: "esarnb, minalk24, matkuh, dchicchon",
+            repo: "https://github.com/dchicchon/Plan-It",
+            description: "A planner app that helps keep track of your day",
+            caption: "Firebase, APIs, JQuery, HTML5, CSS"
+          },
+          {
+            image: spaceTrivia,
+            name: "Space Trivia",
+            link: "https://spacetrivia.herokuapp.com/",
+            authors: "dchicchon",
+            repo: "",
+            description: "Test your knowledge of the Solar System!",
+            caption: "JQuery, HMTL5, CSS, MongoDB"
+          },
+          {
+            image: "",
+            name: "Planner",
+            link:
+              "https://chrome.google.com/webstore/detail/planner/meajimhgfmioppbkoppphhkbcmapfngh",
+            authors: "dchicchon",
+            repo: "https://github.com/dchicchon/planner_extension",
+            description: "A planner to help plan your future!",
+            caption: "HTML5, CSS, Chrome Dev Tools"
+          }
+        ]
+      });
+    } else {
+      this.setState({
+        projectList: [
+          {
+            image: gittrackSmall,
+            name: "GitTrack",
+            link: "http://www.gittrack.ml/",
+            authors: "dchicchon",
+            repo: "https://github.com/dchicchon/GitTrack",
+            description:
+              " A commit tracking site for instructors to keep track of student progress.",
+            caption: "React.js, MySQL, Victory.js, Passport.js"
+          },
+          {
+            image: plannitSmall,
+            name: "PlanIt",
+            link: "http://planit.ml/",
+            authors: "esarnb, minalk24, matkuh, dchicchon",
+            repo: "https://github.com/dchicchon/Plan-It",
+            description: "A planner app that helps keep track of your day",
+            caption: "Firebase, APIs, JQuery, HTML5, CSS"
+          },
+          {
+            image: spaceTriviaSmall,
+            name: "Space Trivia",
+            link: "https://spacetrivia.herokuapp.com/",
+            authors: "dchicchon",
+            repo: "",
+            description: "Test your knowledge of the Solar System!",
+            caption: "JQuery, HMTL5, CSS, MongoDB"
+          },
+          {
+            image: "",
+            name: "Planner",
+            link:
+              "https://chrome.google.com/webstore/detail/planner/meajimhgfmioppbkoppphhkbcmapfngh",
+            authors: "dchicchon",
+            repo: "https://github.com/dchicchon/planner_extension",
+            description: "A planner to help plan your future!",
+            caption: "HTML5, CSS, Chrome Dev Tools"
+          }
+        ]
+      });
+    }
   }
 
   render() {
@@ -86,7 +130,7 @@ class Featured extends React.Component {
                   <Card.Img variant="top" src={project.image} />
                   <Card.Body>
                     <Card.Title>{project.name}</Card.Title>
-                    <Card.Subtitle style={{color: 'grey'}}>
+                    <Card.Subtitle style={{ color: "grey" }}>
                       By {project.authors}
                     </Card.Subtitle>
                     <Card.Text>{project.description}</Card.Text>
