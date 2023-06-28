39299588c67a7327a4aceedab2ecbcf33e253780
Daniel Chicchon <danielchicchon@gmail.com>
2023-06-28 13:48:59 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    esbuild: {
-        // drop: ['console', 'debugger']
-    },
-    plugins: [
-        react(),
-    ],
+export default defineConfig(({ command, mode }) => {
+
+    if (mode === 'production') {
+        return {
+            esbuild: {
+                drop: ['console', 'debugger']
+            },
+            plugins: [
+                react(),
+            ],
+        }
+    }
+    return {
+        plugins: [
+            react(),
+        ],
+    }
+
+
 });
